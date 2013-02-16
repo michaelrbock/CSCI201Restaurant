f5b325efdb8b607db5585c7c6f9627fc1712a825
Michael Bock <mykel.bock@gmail.com>
2013-02-16 01:04:41 -0800

    Menu

--- src/restaurant/Menu.java
+++ src/restaurant/Menu.java
@@ -1,13 +1,26 @@
 package restaurant;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class Menu {
 
-    public String choices[] = new String[]
-	{ "Steak"  ,
-	  "Chicken", 
-	  "Salad"  , 
-	  "Pizza"  };
+	public Map<String,Double> choicesMap = new HashMap<String,Double>(4);
 
+	public String choices[] = new String[]
+			{ "Steak"  ,
+			"Chicken", 
+			"Salad"  , 
+			"Pizza"  };
+
+	public double prices[] = new double[] {15.99, 10.99, 5.99, 8.99};
+	
+	public Menu() {
+		choicesMap.put("Steak", 15.99);
+		choicesMap.put("Chicken", 10.99);
+		choicesMap.put("Salad", 5.99);
+		choicesMap.put("Pizza", 8.99);
+	}
 }
-    
+
